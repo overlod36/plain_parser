fe32bfdd598b77e6fb5ba10d63f083142fdb0c9f
kirbi <Biehan@mail.ru>
2022-09-19 00:32:33 +0300

    now we take a game's title from each page of game, where is  all info

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ for i in range(1, 2663):
     game_shit = flexer.find_all('div', {"class": "game_search"})
 
     for sh_elem in game_shit:
-        print(sh_elem.findChildren('a', recursive=False)[0]['title'].replace(' - дата выхода', ''))
+        game_name = sh_elem.findChildren('a', recursive=False)[0]
+        #print(game_name['title'].replace(' - дата выхода', ''))
+        game_resp = requests.get('https://vgtimes.ru/' + game_name['href'])
+        g_html = BeautifulSoup(game_resp.text, 'lxml')
+        main_el = g_html.find('div', {'class': 'game_header'})
+        print(main_el.findChildren('h1', recursive=False)[0].text)
     
