31f63ae1414472e69fa6ea72fe065089b198e35f
kirbi <Biehan@mail.ru>
2022-09-19 21:48:01 +0300

    now parser also picks release date and developers crew name

--- app.py
+++ app.py
@@ -9,19 +9,30 @@ def get_pic_name(url):
 
 def get_pic(div):
     url = div.findChildren('a', recursive=False)[0].findChildren('div', recursive=False)[0].findChildren('img', recursive=False)[0]['src']
-    with open('img/' + get_pic_name(url), 'wb') as st:
-        response = requests.get('https://vgtimes.ru' + url, stream=True)
+    if url != '/img/no_game_img_mini.jpg':
+        with open('img/' + get_pic_name(url), 'wb') as st:
+            response = requests.get('https://vgtimes.ru' + url, stream=True)
 
-        for block in response.iter_content(1024):
-            if not block:
-                break
+            for block in response.iter_content(1024):
+                if not block:
+                    break
 
-            st.write(block)
+                st.write(block)
 
 def get_title(pg):
     g_html = BeautifulSoup(pg.text, 'lxml')
     main_el = g_html.find('div', {'class': 'game_header'})
     print(main_el.findChildren('h1', recursive=False)[0].text)
+
+def get_info(pg):
+    g_html = BeautifulSoup(pg.text, 'lxml')
+    elems = g_html.find_all('div', {'class': 'rads'})
+    if elems[0].findChildren('div', {'class': 'game_rank'}):
+        print(elems[1].findChildren('div', recursive=False)[0].findChildren('a', recursive=False)[0].text) #дата выхода
+        print(elems[2].findChildren('div', recursive=False)[0].findChildren('a', recursive=False)[0].text) #разработчик
+    else:
+        print(elems[0].findChildren('div', recursive=False)[0].findChildren('a', recursive=False)[0].text) #дата выхода
+        print(elems[1].findChildren('div', recursive=False)[0].findChildren('a', recursive=False)[0].text) #разработчик
 
 def get_all():
     prep_fl = BeautifulSoup(requests.get('https://vgtimes.ru/games/release-dates/all/sort-date/alltime/').text, 'lxml')
@@ -35,6 +46,7 @@ def get_all():
         for sh_elem in game_shit:
             page = requests.get('https://vgtimes.ru/' + sh_elem.findChildren('a', recursive=False)[0]['href'])
             get_title(page)
+            get_info(page)
             get_pic(sh_elem)
 
 if __name__ == '__main__':
